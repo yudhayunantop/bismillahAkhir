cfb3269f8c2728bafc82d1e2433dda644efb94f9
yudhayunantop <yudha.yunanto.2010@gmail.com>
2021-10-10 15:39:31 +0700

    finishing tampilan

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -33,7 +33,7 @@
         </li>
     </ul>
     <ul class="navbar-nav ml-auto">
-        <li class="nav-item dropdown">
+        <!-- <li class="nav-item dropdown">
             <a class="nav-link" data-toggle="dropdown" href="#">
                 <i class="far fa-user"></i>
             </a>
@@ -42,6 +42,6 @@
                     Logout
                 </a>
             </div>
-        </li>
+        </li> -->
     </ul>
 </nav>
--- app/Views/templates/sidebar.php
+++ app/Views/templates/sidebar.php
@@ -1,5 +1,5 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <a href="<?php echo base_url('/'); ?>" class="brand-link">
+    <a href="#" class="brand-link">
       <img src="<?php echo base_url('themes/dist'); ?>/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">Yuda Pratama</span>
